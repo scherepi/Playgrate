f70d1107e3b9104eaeff97568dabafe6a58703b7
scherepi <48649270+scherepi@users.noreply.github.com>
2025-05-28 14:04:52 -0500

    APPLE MUSIC SCRAPE FINALLY DONE

--- apple_music.py
+++ apple_music.py
@@ -1,10 +1,16 @@
 import requests
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+import json
+
+# written by j. schere in may of 2025, with a whole lot of mate and a little bit of love
+
+# also shoutout to the random dude on Reddit whose kinda trash playlist i used as sample data
+# https://music.apple.com/ca/playlist/scrobbleradio-mix/pl.u-dkelCypyBM
 
 # this module is for scraping playlists from Apple Music!
-
-
+# this scrapes the playlist data directly from the front-end HTML of the Apple Music web player.
+# at first i thought i might have to reverse engineer the API or at least use Selenium to get the async data, but in the end it was all in a script at the bottom, thank god
 
 def scrapePlaylist(playlistURL):
     # First, we double-check that the URL is for a valid Apple Music Playlist.
@@ -24,11 +30,26 @@ def scrapePlaylist(playlistURL):
     playlist = {
         "name": playlist_name,
         "song_count": song_count,
+        "image_url": playlist_image_url,
         "songs": {
 
         }
     }
     track_number = 1
-    for song in soup.find_all(property="music:song"):
-        song_dictionary = {}
-        
+    # Find the script tag that contains the playlist data, parse its contents and put them in a dictionary
+    jsonDict = json.loads(soup.find("script", id="serialized-server-data").string)[0]
+    songsDict = jsonDict['data']['sections'][1]['items']
+    for song in songsDict:
+        artist_name = song['artistName']
+        song_name = song['title']
+        album_name = song['tertiaryLinks'][0]['title']
+        playlist['songs'][str(track_number)] = {
+            "artist": artist_name,
+            "name": song_name,
+            "album": album_name,
+        }
+        track_number += 1
+    # Now we can return the playlist dictionary
+    return playlist
+
+print(scrapePlaylist("https://music.apple.com/ca/playlist/scrobbleradio-mix/pl.u-dkelCypyBM"))
