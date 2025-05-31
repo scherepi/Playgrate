beacec2c2103b14c8a954499c7a9c2cd9864db64
scherepi <scherejoaquin@gmail.com>
2025-05-30 19:17:28 -0500

    FINALLY GOT THE STUPID THING TO BUILD

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,6 +2,9 @@
     import LogoCard from "$lib/LogoCard.svelte"
 
 </script>
+<script context="module">
+    export const prerender = false;
+</script>
 <style>
 
 </style>
@@ -10,3 +13,4 @@
 
 <a href="/spotify-login">Click here to log into Spotify</a>
 <a href="/start-scrape/apple-music/?playlistURL=https://music.apple.com/ca/playlist/scrobbleradio-mix/pl.u-dkelCypyBM">Test scrape</a>
+<a href="/view">Try to go to the view page</a>
--- client/src/routes/session-test/+page.svelte
+++ client/src/routes/session-test/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    let playlist_id = fetch("/test-session")
+    async function getPlaylistID() {
+        const url = "/"
+    }
 </script>
 <h1>This will ideally have the current playlist_id listed below.</h1>
 <p>{playlist_id}</p>
