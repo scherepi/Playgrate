5965ff1a4eaa102b67faf22658adc7dd28e9a4df
scherepi <scherejoaquin@gmail.com>
2025-06-02 22:50:42 -0500

    Removed /view route, will re-implement later

--- client/src/routes/view/+page.svelte
+++
@@ -1,7 +0,0 @@
-<!-- i think i'm gonna have to use a big div to color the background... ugh.-->
- <script lang="ts">
-    let playlist_id = $state("placeholder")
-    async function getPlaylistID() {
-        const url = "/test-session"
-    }
- </script>
